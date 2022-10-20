d5c33731a66d4ed0582a46f4e332cd26cadf3339
Michael Pontus <m.pontus@gmail.com>
2022-10-20 13:41:15 +0000

    Design Hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,44 @@
 import type { NextPage } from "next";
+import Image from "next/image";
+import { Col, Row } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import heroImage from "../lib/hero.png";
 
 const Home: NextPage = () => {
   return (
-    <Navbar bg="light" expand="md">
+    <>
+      <Navbar
+        bg="light"
+        expand="md"
+        className="p-0 my-4"
+        style={{ height: 48 }}
+      >
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <Row className="justify-content-md-center py-4 my-8">
+          <Col xs lg="5" className="d-flex align-items-center px-3">
+            <h1 className="display-5">
+              In oculis quidem rerum facilis est et aperta.
+            </h1>
+          </Col>
+          <Col xs lg="5">
+            <Image src={heroImage} alt="Hero image" width={516} height={384} />
+          </Col>
+        </Row>
       </Container>
-    </Navbar>
+    </>
   );
 };
 
